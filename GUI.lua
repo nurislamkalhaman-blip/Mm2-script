68d22734765e7f27527a5873eaa4bc3f46d9077d
Giganiga8374 <nurislamkalhaman@gmail.com>
2026-07-31 16:13:52 +0500

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -2,15 +2,17 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
 
+local ExistingGui = PlayerGui:FindFirstChild("UiName")
+
+if ExistingGui then
+ExistinfGui:Destroy()
+
+else
+
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.ResetOnSpawn = false
 ScreenGui.Name = "UiName"
 ScreenGui.Parent = PlayerGui
-
-if ScreenGui:FindFirstChild("Frame1") then
-ScreenGui.Frame1:Destroy()
-else
-    
 
 local Frame = Instance.new("Frame")
 Frame.Name = "Frame1"
@@ -21,3 +23,4 @@ Frame.BorderColor3 = Color3.FromRGB(255,255,255)
 Frame.AnchorPoint = Vector2.new(0.5,0.5)
 Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
 Frame.Size = UDim2.new(0,400,0,300)
+end
