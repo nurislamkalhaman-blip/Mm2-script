2dc6ff2660d3286974975c5557c23a0245033c4b
Giganiga8374 <nurislamkalhaman@gmail.com>
2026-07-31 16:06:24 +0500

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -7,9 +7,17 @@ ScreenGui.ResetOnSpawn = false
 ScreenGui.Name = "UiName"
 ScreenGui.Parent = PlayerGui
 
+if ScreenGui:FindFirstChild("Frame1") then
+ScreenGui.Frame1:Destroy()
+else
+    
+
 local Frame = Instance.new("Frame")
 Frame.Name = "Frame1"
 Frame.BackgroundColor3 = Color3.FromRGB(0,0,0)
 Frame.Parent = ScreenGui
 Frame.BorderSizePixel = 1
-Frame.BorderColor3 = Color3.FromRGB
+Frame.BorderColor3 = Color3.FromRGB(255,255,255)
+Frame.AnchorPoint = Vector2.new(0.5,0.5)
+Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+Frame.Size = UDim2.new(0,400,0,300)
